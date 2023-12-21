366dc382ab68ba13736c7e76349d18dbef5bb4b6
Karan Dodeja <kdodeja62@gmail.com>
2023-12-22 01:26:17 +0530

    Retriveing Router Data

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,9 +3,9 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
 import { Observable } from 'rxjs';
-import { delay, map } from 'rxjs/operators';
+import { delay, map, filter } from 'rxjs/operators';
 import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationEnd } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -31,6 +31,9 @@ export class AuthService {
     this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(
       delay(1000)
     )
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
+    ).subscribe(console.log)
   }
 
   public async createUser(userData: IUser) {
