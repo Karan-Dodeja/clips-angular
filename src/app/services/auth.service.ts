5205215e42bfc161e80687e2a46047e0df1b8e1c
Karan Dodeja <kdodeja62@gmail.com>
2023-12-22 01:34:24 +0530

    Retrieving data from outlet

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
-import { Observable } from 'rxjs';
-import { delay, map, filter } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { delay, map, filter, switchMap } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { ActivatedRoute, NavigationEnd } from '@angular/router';
 
@@ -15,6 +15,7 @@ export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>
   public isAuthenticated$: Observable<boolean>
   public isAuthenticatedWithDelay$: Observable<boolean>
+  public redirect = false;
 
   constructor(
     private auth: AngularFireAuth,
@@ -32,8 +33,12 @@ export class AuthService {
       delay(1000)
     )
     this.router.events.pipe(
-      filter(e => e instanceof NavigationEnd)
-    ).subscribe(console.log)
+      filter(e => e instanceof NavigationEnd),
+      map(e => this.route.firstChild), // store the current route the user is on
+      switchMap(route => route?.data ?? of({ authOnly: false }))
+    ).subscribe((data) => {
+      this.redirect = data.authOnly ?? false;
+    })
   }
 
   public async createUser(userData: IUser) {
@@ -66,6 +71,10 @@ export class AuthService {
       $event.preventDefault()
     }
     await this.auth.signOut()
-    await this.router.navigateByUrl('/')
+    
+    if (this.redirect) {
+      await this.router.navigateByUrl('/')
+    }
+    
   }
 }
