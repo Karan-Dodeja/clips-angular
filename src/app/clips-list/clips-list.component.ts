577af0efe38a2c950ece38370ff0f738a60e14e7
Karan Dodeja <kdodeja62@gmail.com>
2023-12-24 18:22:30 +0530

    Rendering clips on homepage

--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ClipService } from '../services/clip.service';
+
+
 
 @Component({
   selector: 'app-clips-list',
@@ -7,7 +10,9 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 })
 export class ClipsListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(public clipservice: ClipService) { 
+    clipservice.getClips()
+   }
 
   ngOnInit(): void {
     window.addEventListener('scroll',this.handleScroll)
@@ -21,7 +26,7 @@ export class ClipsListComponent implements OnInit, OnDestroy {
     const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight
 
     if(bottomOfWindow) {
-      
+        this.clipservice.getClips()
     }
 
   }
